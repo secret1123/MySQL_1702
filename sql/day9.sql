26dfb28141dc86d15dc5b8cc6f5b6219d19f5902
secret1123 <19072390@qq.com>
2017-05-26 09:01:32 +0800

    update

--- sql/day9.sql
+++ sql/day9.sql
@@ -1,2 +1,22 @@
 SELECT *
 FROM mysql.user;
+
+SELECT *
+FROM scott.emp;
+
+GRANT CREATE ON scott.* TO 'tester'@'localhost';
+GRANT SELECT ON scott.* TO 'tester'@'localhost';
+GRANT DROP ON scott.* TO 'tester'@'localhost';
+GRANT ALL ON scott.* TO 'tester'@'localhost';
+
+REVOKE SELECT ON scott.* FROM 'tester'@'localhost';
+REVOKE DROP ON scott.* FROM 'tester'@'localhost';
+
+RENAME USER tester@'localhost' TO tester_new;
+
+DROP USER tester_new@'%';
+
+CREATE DATABASE db_test;
+CREATE USER 'tester'@'localhost'
+IDENTIFIED BY 'test123';
+GRANT ALL ON db_test.* TO 'tester'@'localhost';
